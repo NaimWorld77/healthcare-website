951797fe21d6db5a35870d70bb35b00e7b1d0117
NaimWorld77 <abdunaim2018nuist@gmail.com>
2021-10-19 10:22:55 +0600

    2nd commit

--- src/Components/DetailsService/DetailsService.js
+++ src/Components/DetailsService/DetailsService.js
@@ -1,18 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const DetailsService = () => {
     const {serviceId} = useParams();
+    const [details,setDetails] = useState([]);
+   
 
    useEffect(()=>{
-       fetch(`services/${serviceId}`)
+       fetch('/allservices.json')
        .then(res=>res.json())
-       .then(data=>console.log(data))
+       .then(data=>setDetails(data))
    },[]);
 
+   const serviceDetails = details.find(deatial=>deatial.id == serviceId);
+
+
     return (
-        <div>
-            <h3>this is single details page {serviceId}</h3>
+        <div className="my-4">
+            <div className="row shadow-lg p-3 m-5 bg-body rounded">
+                <div className="col-md-12 ">
+                <h2><span className="text-primary">All details of this section</span></h2>
+                <img className="rounded" width="50%" height="50%" src={serviceDetails?.img} alt="" />
+                <h3><span className="text-danger">{serviceDetails?.name}</span></h3>
+                <h5 className="">{serviceDetails?.discription}</h5>
+                <Link to={`/services`}>
+                <button className="btn btn-primary mt-3">Go Previous</button>
+                </Link>
+            
+                </div> 
+            </div>
         </div>
     );
 };
